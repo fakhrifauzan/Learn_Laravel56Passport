6b6ae4bcdb3aa7eeca914a7522d100168848fde0
Fakhri Fauzan <fazan697@gmail.com>
2018-09-19 11:43:02 +0700

    add login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,8 @@ class AuthController extends Controller
      * @return [string] message
      */
 
-    public function signup(Request $request){
+    public function signup(Request $request) 
+    {
       $request->validate([
         'name' => 'required|string',
         'email' => 'required|string|email|unique:users',
@@ -38,4 +39,51 @@ class AuthController extends Controller
         'message' => 'Successfully created user!'
       ], 201);
     }
+
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(Request $request) 
+    {
+      $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+        'remember_me' => 'boolean'
+      ]);
+
+      $credentials = request(['email', 'password']);
+
+      if (!Auth::attempt($credentials)) {
+        return response()->json([
+          'message' => 'Unauthorized'
+        ], 401);
+      }
+
+      $user = $request->user();
+
+      $tokenResult = $user->createToken('Personal Access Token');
+      $token = $tokenResult->token;
+
+      if ($reuqest->remember_me) {
+        $token->expires_at = Carbon::now()->addWeeks(1);
+      }
+
+      $token->save();
+
+      return response()->json([
+        'access_token' => $tokenResult->accessToken,
+        'token_type' => 'Bearer',
+        'expires_at' => Carbon::parse(
+          $tokenResult->token->expires_at
+        )->toDateTimeString()
+      ]);
+    }
+
 }
